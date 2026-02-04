67f170992cb3f618393e81eb9e3f901fcd607937
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-05 00:59:02 +0200

    created categories and menu-items views,models,serializers

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Category, MenuItem
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+        
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'featured', 'category', 'category_id']
