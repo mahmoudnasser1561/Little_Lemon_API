5be6a8bbf983733e549c766750d79a7313e06fd5
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-05 02:00:35 +0200

    Cart management

--- restaurant/models.py
+++ restaurant/models.py
@@ -12,3 +12,13 @@ class MenuItem(models.Model):
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     def __str__(self): return self.title
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('menuitem', 'user')
