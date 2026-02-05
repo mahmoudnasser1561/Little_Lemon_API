c318d70b5429d0329acdf9254e3a2d3e05ba69cd
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-05 02:18:35 +0200

    order management

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -41,4 +41,14 @@ urlpatterns = [
         'cart/menu-items', 
         views.CartView.as_view()
     ),
+    
+    path(
+        'orders', 
+        views.OrderView.as_view()
+    ),
+    
+    path(
+        'orders/<int:pk>', 
+        views.SingleOrderView.as_view()
+    ),
 ]
