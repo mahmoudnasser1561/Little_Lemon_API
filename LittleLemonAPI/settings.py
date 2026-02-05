5587af88474124d89583526fcba6c4bc0af048b3
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-05 03:03:22 +0200

    added pagination

--- LittleLemonAPI/settings.py
+++ LittleLemonAPI/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     "DEFAULT_THROTTLE_RATES": {
         "anon": "20/min",
         "user": "100/min",
