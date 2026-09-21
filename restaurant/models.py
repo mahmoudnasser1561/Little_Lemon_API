4829be3086c931431c2197128b1219da10552073
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-21 23:49:28 +0300

    Add ManagerUser and DeliveryCrewUser role models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models, transaction
+from django.contrib.auth.models import User, Group, UserManager
+from django.contrib.auth.password_validation import validate_password
 
 class Category(models.Model):
     slug = models.SlugField()
@@ -39,3 +40,39 @@ class OrderItem(models.Model):
 
     class Meta:
         unique_together = ('order', 'menuitem')
+
+# Staff roles: a User that belongs to the role's group, created and validated through the role model
+class RoleUserManager(UserManager):
+    use_in_migrations = False
+    role = None
+
+    def get_queryset(self):
+        return super().get_queryset().filter(groups__name=self.role)
+
+    def create_user(self, username, password, email=None, **extra_fields):
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        validate_password(password, user)
+        user.set_password(password)
+        user.full_clean()
+        with transaction.atomic():
+            user.save(using=self._db)
+            user.groups.add(Group.objects.get(name=self.role))
+        return user
+
+class ManagerUserManager(RoleUserManager):
+    role = 'Manager'
+
+class DeliveryCrewUserManager(RoleUserManager):
+    role = 'Delivery Crew'
+
+class ManagerUser(User):
+    objects = ManagerUserManager()
+
+    class Meta:
+        proxy = True
+
+class DeliveryCrewUser(User):
+    objects = DeliveryCrewUserManager()
+
+    class Meta:
+        proxy = True
